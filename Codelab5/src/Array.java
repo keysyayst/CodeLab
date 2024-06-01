07dc0f8bd8c01e0e620954521680512278cc3908
keysyayst <keysyayesanty123@gmail.com>
2024-06-01 22:43:56 +0700

    cd6.5

--- Codelab5/src/Array.java
+++ Codelab5/src/Array.java
@@ -1,29 +1,28 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class ListMahasiswa {
+public class Array {
     public static void main(String[] args) {
-        ArrayList<String> namaMahasiswa = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> namaMahasiswa = new ArrayList<>();
 
         int i = 1;
         while (true) {
             System.out.print("Masukkan nama ke-" + i + ": ");
-            String namaInput = scanner.nextLine();
-            if (namaInput.trim().isEmpty()) {
+            String nama = scanner.nextLine();
+
+            if (nama.trim().isEmpty()) {
                 try {
-                    throw new IllegalArgumentException("Nama tidak boleh kosong!");
+                    throw new IllegalArgumentException("Nama tidak boleh kosong");
                 } catch (IllegalArgumentException e) {
                     System.out.println(e.getMessage());
                     continue;
                 }
             }
-            if (namaInput.equalsIgnoreCase("selesai")){
+            if (nama.equalsIgnoreCase("selesai")) {
                 break;
-
             }
-
-            namaMahasiswa.add(namaInput);
+            namaMahasiswa.add(nama);
             i++;
         }
 
@@ -31,5 +30,5 @@ public class ListMahasiswa {
         for (String nama : namaMahasiswa) {
             System.out.println(nama);
         }
-    }
+    }    
 }
